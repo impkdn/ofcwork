30eaa230661312e463c2cf0e389740626338477a
impkdn <piyush.jld@gmail.com>
2022-07-18 01:33:31 +0530

    this is done in home

---
+++ index.js
@@ -0,0 +1,6 @@
+function openSidebar() {
+    document.getElementById("icon-toggle").style.width = "287px";
+    document.getElementById("header-container").style.marginLeft = "287px";
+    // document.getElementById("icon-toggle").style.display = "none";
+}
+
