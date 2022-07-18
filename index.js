6acc7ac31789649b5133f18681849bd1509c5509
impkdn <piyush.jld@gmail.com>
2022-07-18 18:26:09 +0530

    18-07-2022 update made in office

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function openSidebar() {
-    document.getElementById("icon-toggle").style.width = "287px";
-    document.getElementById("header-container").style.marginLeft = "287px";
+// function openSidebar() {
+    // document.getElementById("icon-toggle").style.display = "inline-block";
+    // document.getElementById("header-container").style.width = "1550px";
     // document.getElementById("icon-toggle").style.display = "none";
-}
+// }
 
