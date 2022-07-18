412e194e7c82e914e42093ef056e550f352eb855
impkdn <piyush.jld@gmail.com>
2022-07-18 19:12:22 +0530

    this is seccond commit after updating js in office 18/07/2022

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // function openSidebar() {
-    // document.getElementById("icon-toggle").style.display = "inline-block";
-    // document.getElementById("header-container").style.width = "1550px";
-    // document.getElementById("icon-toggle").style.display = "none";
+//     document.getElementById("icon-toggle").style.width = "287px";
+//     document.getElementById("header-container").style.width = "1550px";
+//     // console.log("Hiii");
+//     // document.getElementById("icon-toggle").style.display = "none";
 // }
 
+$(document).ready(function(){
+    $("#contact").click( function(){
+        $("#icon-toggle").css("width","287px");
+        $(".hide-content").show(800);
+        $("#header-container").css("width","1550px")
+        
+    })
+}) ;
